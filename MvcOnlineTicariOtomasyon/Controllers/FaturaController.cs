c731cd492b5b7504cee4337114845afd54cc2507
aliberkaygdk <aliberkaygdk@gmail.com>
2024-10-16 15:05:05 +0300

    faturalar okuma,ekleme,güncelleme. fatura kalem ekleme

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -16,5 +16,55 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var liste = c.Faturalar.ToList();
             return View(liste);
         }
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FaturaEkle(Faturalar f)
+        {
+            c.Faturalar.Add(f);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult FaturaGetir(int id)
+        {
+            var fatura = c.Faturalar.Find(id);
+            return View("FaturaGetir", fatura);
+            // return View("FaturaGetir",fatura);
+
+        }
+        public ActionResult FaturaGuncelle(Faturalar f)
+        {
+            var fatura = c.Faturalar.Find(f.Faturaid);
+            fatura.FaturaSeriNo = f.FaturaSeriNo;
+            fatura.FaturaSıraNo = f.FaturaSıraNo;
+            fatura.Saat=f.Saat;
+            fatura.FaturaTarih = f.FaturaTarih;
+            fatura.TeslimAlan = f.TeslimAlan;
+            fatura.TeslimEden = f.TeslimEden;
+            fatura.VergiDairesi = f.VergiDairesi;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult FaturaDetay(int id)
+        {
+            var degerler = c.FaturaKalem.Where(x => x.Faturaid == id).ToList();
+            
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKalem()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKalem(FaturaKalem p)
+        {
+            c.FaturaKalem.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
